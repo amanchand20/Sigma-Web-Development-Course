8b6e7fdcdd3b8a068c6b89e18aa28d2d60455a63
Aman <amanchand00000@gmail.com>
2026-07-15 13:05:34 +0530

    Node.js

---
+++ 054: Introduction of JavaScript/script.js
@@ -0,0 +1,19 @@
+alert("Hello World")
+
+console.log("Code is running...")
+console.log("Code is also running...")
+console.log("Code is running like a wow...")
+
+var a = prompt("Enter your number")
+
+var isTrue = confirm("You want to leave this page and blast your computer")
+
+if(isTrue){
+    console.log("Computer is blasting...")
+}
+
+console.log("Your number is " + a)
+
+document.title = "Hey I'm good"
+
+document.body.style.backgroundColor = "red"
