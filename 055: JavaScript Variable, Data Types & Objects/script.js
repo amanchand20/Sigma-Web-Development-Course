aed14e50b63779a917d090685cd4ba847777e6d0
Aman <amanchand00000@gmail.com>
2026-07-16 08:13:31 +0530

    Suiiii

---
+++ 055: JavaScript Variable, Data Types & Objects/script.js
@@ -0,0 +1,43 @@
+console.log("Hello World")
+
+let a = 5;
+let b = 6;
+let c = "Aman"
+let _a = "Shubham";
+// var 98a = 45;
+
+// console.log(a + b)
+
+// console.log(typeof a, typeof b, typeof c)
+
+{
+    let a = 66;
+    console.log(a)
+}
+
+    console.log(a)
+
+// const a45 = 34;
+// a45 = a45 + 1; (Not allowed bcz a45 is constant)
+
+let x = "Aman"
+let y = 23;
+let z = 2.35;
+let p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    name : "Aman",
+    "job code" : 34928,
+    "is_handsome" : true;
+}
+
+console.log(o)
+o.salary = "100crores";
+console.log(o)
+o.salary = "500crores";
+console.log(o)
