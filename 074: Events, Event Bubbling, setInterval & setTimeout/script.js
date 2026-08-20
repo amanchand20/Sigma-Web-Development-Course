a9e1e2679500759e462a2e8b6c01f74bbad1b4f3
Aman <amanchand00000@gmail.com>
2026-08-20 21:36:55 +0530

    add

--- 074: Events, Event Bubbling, setInterval & setTimeout/script.js
+++ 074: Events, Event Bubbling, setInterval & setTimeout/script.js
@@ -1,5 +1,9 @@
 let button = document.getElementById("btn")
 
-button.addEventListener("click", ()=>{
-    
+button.addEventListener("dblclick", () => {
+    document.querySelector(".box").innerHTML = "<b>Yayyy you were clicked</b> Enjoy your click!"
+})
+
+button.addEventListener("dblclick", () => {
+    document.querySelector(".box").innerHTML = "<b>Yayyy you were clicked</b> Enjoy your click!"
 })
