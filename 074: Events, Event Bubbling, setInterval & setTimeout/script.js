fa49ccbc7ead4b635de82edbb5dcf036ac324ec1
Aman <amanchand00000@gmail.com>
2026-08-21 18:09:19 +0530

    074 complete

--- 074: Events, Event Bubbling, setInterval & setTimeout/script.js
+++ 074: Events, Event Bubbling, setInterval & setTimeout/script.js
@@ -7,3 +7,11 @@ button.addEventListener("dblclick", () => {
 button.addEventListener("dblclick", () => {
     document.querySelector(".box").innerHTML = "<b>Yayyy you were clicked</b> Enjoy your click!"
 })
+
+document.addEventListener("keydown", (e)=>{
+    console.log(e, e.key, e.keyCode)
+})
+
+setInterval(() => {
+    document.querySelector(".childContainer").style.background = "red";
+}, 5000);
